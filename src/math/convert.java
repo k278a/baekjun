f6c0e7b3bac07a11e79bc9b921e51a4e6d92dbcb
USER K <ksar1030k@gmail.com>
2024-07-13 22:55:10 +0900

    convert

---
+++ src/math/convert.java
@@ -0,0 +1,12 @@
+package math;
+
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        int answer = 0;
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        answer = input - 543;
+        System.out.println(answer);
+    }
+}
